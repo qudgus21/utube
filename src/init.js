67a03f89da209c1255ce0d5df5f157f768f57a7e
qudgus21 <47776356+qudgus21@users.noreply.github.com>
2019-06-02 23:09:23 +0900

    asf
    safs

--- src/init.js
+++ src/init.js
@@ -5,6 +5,8 @@ import "@babel/polyfill";
 import "./models/Video";
 import "./models/Comment";
 import "./models/User";
+import "babel-core/register";
+import "babel-polyfill";
 
 dotenv.config();
 
